a3c430be85b722fe19bcca1de2c5376f03169a1d
ammarahmad <ammar.ahmad6410@gmail.com>
2024-04-02 14:09:12 +0400

    add login page ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "antd/dist/reset.css";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
+import { ConfigProvider } from "antd";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider
+      theme={{ token: { colorPrimary: "#F65F42", colorLink: "#F65F42" } }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  </React.StrictMode>
+);
